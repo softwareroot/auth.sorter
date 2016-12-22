17a8a823e671c6786fce4ade0c882a2d395cbe74
softwareroot <rootgam3s@gmail.com>
2016-12-22 19:01:52 +0100

    Pridavam uzivatelske graficke rozhranie.

--- MusicSorter/src/com/root/musicsorter/MusicSorter.java
+++ MusicSorter/src/com/root/musicsorter/MusicSorter.java
@@ -5,7 +5,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+
 import org.apache.commons.io.FileUtils;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JProgressBar;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MusicSorter {
 	
@@ -13,7 +23,45 @@ public class MusicSorter {
 	private static ArrayList<String> menaAutorov = new ArrayList<String>();
 	private static Scanner scanner = new Scanner(System.in);
 	
+	private static String zlozkaHudbaNeutriedena;
+	private static String cielovaZlozka;
+	
+	/*******************************FRAME VARIABLES***********************************/
+	private static JFrame frame;
+	
+	private static JTextPane textPanelLog;
+	
+	private static JTextField absPathUnsortedText;
+	private static JTextField absPathSortedText;
+	
+	private static JLabel lblEnterTheAbsolute;
+	private static JLabel lblAbsolutePathOf;
+	private static JLabel lblLog;
+	private static JButton btnStartSorting;
+	
+	private static JProgressBar progressBarSorting;
+	
+	private static void initJFrame(String title, int width, int height) {
+		frame = new JFrame(title);
+		frame.setSize(600, 240);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
+		
+		initJLables();
+		initJTextFields();
+		initJButtons();
+		initProgressBar();
+		initLogPanel();
+		
+		frame.setVisible(true);
+	}
+	
 	public static void main(String[] args) {
+		initJFrame("Music Sorter", 600, 322);
+		
+		/*
 		String cielovaZlozka = "";
 		
 		boolean spravnaCesta = false;
@@ -26,7 +74,6 @@ public class MusicSorter {
 				cielovaZlozka = scanner.nextLine();
 				
 				vytvorZlozku(cielovaZlozka);
-		    	
 		    	vytvorPoleSoVsetkymiPesnickami(zlozkaHudbaNeutriedena, vsetkyPesnicky);
 				vytvorCieloveZlozky(cielovaZlozka, menaAutorov);
 		    	spravnaCesta = true;
@@ -39,9 +86,10 @@ public class MusicSorter {
 		scanner.close();
 		
 		File[] listZloziekAutorov = new File(cielovaZlozka).listFiles();
-		skopirujPesnickyDoCielovychZloziek(listZloziekAutorov);
+		skopirujPesnickyDoCielovychZloziek(listZloziekAutorov); */
 		System.out.println("Kopirovanie hudby skoncilo.");
 	}
+	
 	
 	private static void vytvorZlozku(String cesta) {
 		File dir = new File(cesta);
@@ -99,6 +147,7 @@ public class MusicSorter {
 					
 					try {
 						System.out.println("Kopirujem... " + vsetkyPesnicky.get(i).getName());
+						textPanelLog.setText("Kopirujem... " + vsetkyPesnicky.get(i).getName() + "\n");
 						FileUtils.copyFile(suborNaSkopirovanie, kamSkopirovat);
 					} catch (IOException e1) {
 						e1.printStackTrace();
@@ -106,5 +155,70 @@ public class MusicSorter {
 				}
 			}
 		}
+	}
+	
+	private static void initJLables() {
+		/*JLabel 0*/
+		lblEnterTheAbsolute = new JLabel("Absolute path of the music folder");
+		lblEnterTheAbsolute.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblEnterTheAbsolute.setBounds(10, 11, 222, 19);
+		frame.getContentPane().add(lblEnterTheAbsolute);
+		
+		/*JTextFiled Absolute path Sorted*/
+		lblAbsolutePathOf = new JLabel("Absolute path of the sorted music");
+		lblAbsolutePathOf.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAbsolutePathOf.setBounds(10, 74, 233, 19);
+		frame.getContentPane().add(lblAbsolutePathOf);
+	}
+	
+	private static void initJTextFields() {
+		/*JTextFiled Absolute path Unsorted*/
+		absPathUnsortedText = new JTextField();
+		absPathUnsortedText.setBounds(10, 31, 320, 20);
+		frame.getContentPane().add(absPathUnsortedText);
+		absPathUnsortedText.setColumns(10);
+		
+		/*JTextFiled Absolute path Sorted*/
+		absPathSortedText = new JTextField();
+		absPathSortedText.setColumns(10);
+		absPathSortedText.setBounds(10, 93, 320, 20);
+		frame.getContentPane().add(absPathSortedText);
+	}
+	
+	private static void initJButtons() {
+		
+		/*JButton Start Sorting*/
+		btnStartSorting = new JButton("Start Sorting");
+		btnStartSorting.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				zlozkaHudbaNeutriedena = absPathUnsortedText.getText();
+				cielovaZlozka = absPathSortedText.getText();
+				
+				vytvorZlozku(cielovaZlozka);
+		    	vytvorPoleSoVsetkymiPesnickami(zlozkaHudbaNeutriedena, vsetkyPesnicky);
+				vytvorCieloveZlozky(cielovaZlozka, menaAutorov);
+				File[] listZloziekAutorov = new File(cielovaZlozka).listFiles();
+				skopirujPesnickyDoCielovychZloziek(listZloziekAutorov);
+			}
+		});
+		btnStartSorting.setBounds(10, 146, 109, 23);
+		frame.getContentPane().add(btnStartSorting);
+	}
+	
+	private static void initProgressBar() {
+		progressBarSorting = new JProgressBar();
+		progressBarSorting.setBounds(129, 146, 201, 24);
+		frame.getContentPane().add(progressBarSorting);
+	}
+	
+	private static void initLogPanel() {
+		textPanelLog = new JTextPane();
+		textPanelLog.setBounds(340, 31, 244, 138);
+		frame.getContentPane().add(textPanelLog);
+		
+		lblLog = new JLabel("Log");
+		lblLog.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblLog.setBounds(340, 12, 30, 19);
+		frame.getContentPane().add(lblLog);
 	}
 }
